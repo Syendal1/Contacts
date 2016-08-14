c6e73a721b40e788291bbbb6a4a4e63ed346fe9e
Syendal1 <snehith.yendal0313@gmail.com>
2016-08-14 13:24:32 -0700

    Contacts

--- Contacts/AppDelegate.m
+++ Contacts/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ViewController *objVC=[[ViewController alloc]init];
+    UINavigationController *navController=[[UINavigationController alloc]initWithRootViewController:objVC];
+    self.window.rootViewController=navController;
+    
     return YES;
 }
 
--- Contacts/ViewController.m
+++ Contacts/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <Contacts/Contacts.h>
+#import <ContactsUI/ContactsUI.h>
+
+@interface ViewController ()<CNContactPickerDelegate, UITableViewDelegate,UITableViewDataSource>
+{
+    NSMutableArray *dataSource;
+    UITableView *table;
+}
 
 @end
 
@@ -16,8 +23,68 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor orangeColor];
+    
+    UIBarButtonItem *add=[[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(addContactSellected)];
+    self.navigationItem.rightBarButtonItem=add;
+    
+    dataSource=[[NSMutableArray alloc]init];
+    
+    table=[[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    table.delegate=self;
+    table.dataSource=self;
+    [self.view addSubview:table];
+    
 }
+
+-(void)addContactSellected{
+    self.view.backgroundColor=[UIColor blueColor];
+    CNContactPickerViewController *peoplePicker=[[CNContactPickerViewController alloc]init];
+    peoplePicker.delegate=self;
+    [self presentViewController:peoplePicker animated:YES completion:nil];
+}
+
+- (void)contactPickerDidCancel:(CNContactPickerViewController *)picker;{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (void)contactPicker:(CNContactPickerViewController *)picker didSelectContactProperty:(CNContactProperty *)contactProperty;{
+    
+    [dataSource addObject:contactProperty];
+    
+    NSLog(@"contactProperty %@",contactProperty);
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
+    [table reloadData];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;{
+    return [dataSource count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;{
+    UITableViewCell *cell=[table dequeueReusableCellWithIdentifier:@"Cell"];
+    if(cell==nil){
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"Cell"];
+    }
+    
+    CNContactProperty *contactProperty=[dataSource objectAtIndex:indexPath.row];
+    cell.textLabel.text=[NSString stringWithFormat:@"%@ %@",contactProperty.contact.givenName,contactProperty.contact.familyName];
+    if ([contactProperty.value isKindOfClass:[NSString class]]) {
+        NSString * value=contactProperty.value;
+        cell.detailTextLabel.text=value;
+    }
+    else if ([contactProperty.value isKindOfClass:[CNPhoneNumber class]]){
+        CNPhoneNumber *phone=contactProperty.value;
+        cell.detailTextLabel.text=phone.stringValue;
+    }//    else if ([contactProperty.value isKindOfClass:[CNPostalAddress class]]){
+//        CNPhoneNumber *phone=contactProperty.;
+//        cell.detailTextLabel.text=phone.str;
+//    }
+    return cell;
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
